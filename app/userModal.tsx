065f9d1dca189b890d130545df89286f03c5821e
matmal <m.mateusz2000@wp.pl>
2024-03-17 18:55:13 +0100

    style: Author details

--- app/userModal.tsx
+++ app/userModal.tsx
@@ -1,24 +1,48 @@
-import { StatusBar } from "expo-status-bar";
-import { Platform, StyleSheet } from "react-native";
+import { StyleSheet } from "react-native";
 
-import EditScreenInfo from "@/components/EditScreenInfo";
 import { Text, View } from "@/components/Themed";
 import { useRoute } from "@react-navigation/native";
+import { useFetchUserDetails } from "@/libs";
+import { ActionsHandler } from "@/components/ActionsHandler";
+import { IUser } from "@/types";
+import Avatar from "@/components/Avatar";
 
 const UserModalScreen = () => {
     const {
         params: { id },
     } = useRoute<any>();
+    const userState = useFetchUserDetails(id);
 
     return (
-        <View style={styles.container}>
-            <Text style={styles.title}>{id}</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="app/modal.tsx" />
-
-            {/* Use a light status bar on iOS to account for the black space above the modal */}
-            <StatusBar style={Platform.OS === "ios" ? "light" : "auto"} />
-        </View>
+        <ActionsHandler<IUser> {...userState}>
+            {(user) => (
+                <View style={styles.container}>
+                    <View style={styles.avatarContainer}>
+                        <Avatar />
+                        <Text style={styles.userName}>{user.name}</Text>
+                    </View>
+                    <Text style={styles.title}>Personal informations</Text>
+                    <View style={styles.userDetailsContainer}>
+                        <View style={styles.row}>
+                            <Text style={styles.header}>Name:</Text>
+                            <Text style={styles.description}>{user.name}</Text>
+                        </View>
+                        <View style={styles.row}>
+                            <Text style={styles.header}>Email:</Text>
+                            <Text style={styles.description}>{user.email}</Text>
+                        </View>
+                        <View style={styles.row}>
+                            <Text style={styles.header}>Company:</Text>
+                            <Text style={styles.description}>{user.company.name}</Text>
+                        </View>
+                        <View style={styles.row}>
+                            <Text style={styles.header}>City:</Text>
+                            <Text style={styles.description}>{user.address.city}</Text>
+                        </View>
+                    </View>
+                </View>
+            )}
+        </ActionsHandler>
     );
 };
 export default UserModalScreen;
@@ -30,12 +54,37 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     title: {
-        fontSize: 20,
+        marginTop: 20,
+        fontSize: 22,
         fontWeight: "bold",
     },
     separator: {
         marginVertical: 30,
         height: 1,
         width: "80%",
+    },
+    avatarContainer: {
+        gap: 20,
+        alignItems: "center",
+    },
+    userName: {
+        fontSize: 30,
+    },
+    userDetailsContainer: {
+        marginTop: 20,
+        alignItems: "center",
+        width: 300,
+    },
+    row: {
+        flexDirection: "row",
+        justifyContent: "space-between",
+        width: "100%",
+    },
+    header: {
+        fontSize: 20,
+        fontWeight: "bold",
+    },
+    description: {
+        fontSize: 20,
     },
 });
