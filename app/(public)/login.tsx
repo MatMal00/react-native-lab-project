7a813264e3485b991af863ac677c83f0929a4e86
matmal <m.mateusz2000@wp.pl>
2024-03-17 18:40:37 +0100

    feat: handle login

--- app/(public)/login.tsx
+++ app/(public)/login.tsx
@@ -1,13 +1,43 @@
-import EditScreenInfo from "@/components/EditScreenInfo";
-import { Text, View } from "@/components/Themed";
-import { StyleSheet } from "react-native";
+import { Text, View, useThemeColor } from "@/components/Themed";
+import { useAuth } from "@/libs";
+import { Formik } from "formik";
+import { Button, GestureResponderEvent, StyleSheet, TextInput } from "react-native";
 
 const LoginScreen = () => {
+    const { login } = useAuth();
+    const color = useThemeColor({ dark: "#fff", light: "#000" }, "text");
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Login</Text>
-            <View style={styles.separator} lightColor="#eee" darkColor="rgba(255,255,255,0.1)" />
-            <EditScreenInfo path="app/(tabs)/login.tsx" />
+            <Formik initialValues={{ email: "", password: "" }} onSubmit={(values) => login(values.email)}>
+                {({ handleChange, handleBlur, handleSubmit, values }) => (
+                    <View style={styles.form}>
+                        <TextInput
+                            style={{ color, ...styles.input }}
+                            onChangeText={handleChange("email")}
+                            onBlur={handleBlur("email")}
+                            value={values.email}
+                            placeholder="E-mail"
+                        />
+                        <TextInput
+                            style={{ color, ...styles.input }}
+                            onChangeText={handleChange("password")}
+                            onBlur={handleBlur("password")}
+                            value={values.password}
+                            secureTextEntry
+                            placeholder="Password"
+                        />
+                        <View style={styles.button}>
+                            <Button
+                                onPress={handleSubmit as (e?: GestureResponderEvent) => void}
+                                title="Submit"
+                                color="#000"
+                            />
+                        </View>
+                    </View>
+                )}
+            </Formik>
         </View>
     );
 };
@@ -20,12 +50,26 @@ const styles = StyleSheet.create({
         justifyContent: "center",
     },
     title: {
-        fontSize: 20,
+        marginBottom: 40,
+        fontSize: 40,
         fontWeight: "bold",
     },
-    separator: {
-        marginVertical: 30,
-        height: 1,
-        width: "80%",
+    form: {
+        padding: 40,
+        display: "flex",
+        gap: 20,
+        width: "100%",
+    },
+    input: {
+        paddingVertical: 10,
+        paddingHorizontal: 20,
+        borderWidth: 1,
+        borderColor: "#c2a83e",
+        borderRadius: 5,
+        fontSize: 25,
+        width: "100%",
+    },
+    button: {
+        backgroundColor: "#c2a83e",
     },
 });
